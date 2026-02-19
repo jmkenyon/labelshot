d07a4508f4f828db2d3be1034cc51fc231e904e0
Josh Kenyon <joshuakenyon1@gmail.com>
2026-02-19 17:08:06 -0300

    correct post to all caps

--- app/api/send-waitlist.ts
+++ app/api/send-waitlist.ts
@@ -2,7 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
-export async function Post(req: Request) {
+export async function POST(req: Request) {
     const { email } = await req.json();
     await resend.emails.send({
     from: "Labelshot <noreply@labelshot.io>",
